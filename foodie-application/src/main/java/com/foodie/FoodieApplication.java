4784caad8c7431fb3c2586e42e8de1a738cddbb5
Praksh671 <prakashsochandrasekar@gmail.com>
2024-08-11 15:41:14 +0530

    review comment changes by SS

--- foodie-application/src/main/java/com/foodie/FoodieApplication.java
+++ foodie-application/src/main/java/com/foodie/FoodieApplication.java
@@ -2,11 +2,9 @@ package com.foodie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan
 
 public class FoodieApplication {
 
